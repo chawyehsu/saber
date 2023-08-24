ffea25f68d31b3e37d31da9142e43c298d25f8f6
Chawye Hsu <su+git@chawyehsu.com>
2023-08-24 23:36:57 +0800

    fix(cli): added node version check
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- packages/saber/src/cli.ts
+++ packages/saber/src/cli.ts
@@ -3,6 +3,18 @@ import { cac } from 'cac'
 import { log } from 'saber-log'
 import commands from './cli-commands'
 
+function isSupported() {
+  const [major] = process.versions.node.split('.', 1).map(Number)
+  return major >= 16 && major < 17
+}
+
+if (!isSupported()) {
+  log.error(
+    `Saber requires Node.js >= 16 and < 17 to work currently, your current Node.js version is ${process.versions.node}.`
+  )
+  process.exit(1)
+}
+
 const cli = cac()
 commands(cli)
 
