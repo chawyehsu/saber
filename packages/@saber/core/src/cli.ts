1013e28972479c8e549e23698ae8cfbe2fe1bbcc
Chawye Hsu <su+git@chawyehsu.com>
2023-08-25 23:52:21 +0800

    refactor(cli): tweak entry bin
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- packages/@saber/core/src/cli.ts
+++ packages/@saber/core/src/cli.ts
@@ -8,32 +8,34 @@ function isSupported() {
   return major >= 16 && major < 17
 }
 
-if (!isSupported()) {
-  log.error(
-    `Saber requires Node.js >= 16 and < 17 to work currently, your current Node.js version is ${process.versions.node}.`
-  )
-  process.exit(1)
+export function start() {
+  if (!isSupported()) {
+    log.error(
+      `Saber requires Node.js >= 16 and < 17 to work currently, your current Node.js version is ${process.versions.node}.`
+    )
+    process.exit(1)
+  }
+
+  const cli = cac()
+  commands(cli)
+
+  cli.option('-V, --verbose', 'Output verbose logs')
+  cli.option('--no-progress', 'Disable progress bar')
+
+  cli.version(require('../package').version)
+
+  cli.help()
+
+  cli.parse()
+
+  process.on('SIGINT', () => {
+    log.log('')
+    log.info(`See you later, master!`) // <-- Saber says
+    process.exit()
+  })
+
+  process.on('unhandledRejection', (error: any) => {
+    log.error(error.stack)
+    process.exitCode = 1
+  })
 }
-
-const cli = cac()
-commands(cli)
-
-cli.option('-V, --verbose', 'Output verbose logs')
-cli.option('--no-progress', 'Disable progress bar')
-
-cli.version(require('../package').version)
-
-cli.help()
-
-cli.parse()
-
-process.on('SIGINT', () => {
-  log.log('')
-  log.info(`See you later, master!`) // <-- Saber says
-  process.exit()
-})
-
-process.on('unhandledRejection', (error: any) => {
-  log.error(error.stack)
-  process.exitCode = 1
-})
