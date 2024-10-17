ac123d64cfa0abe3169bd434346874f72507b6d6
Chawye Hsu <su+git@chawyehsu.com>
2024-10-17 11:55:04 +0800

    chore: lint
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- packages/@alterjs/saber-markdown/babel-plugin-vue-features.mjs
+++ packages/@alterjs/saber-markdown/babel-plugin-vue-features.mjs
@@ -1,7 +1,7 @@
-import { types as t } from "@babel/core"
-import { declare } from "@babel/helper-plugin-utils"
+import { types as t } from '@babel/core'
+import { declare } from '@babel/helper-plugin-utils'
 
-export default declare(api => {
+export default declare((api) => {
   api.assertVersion(7)
 
   return {
@@ -10,14 +10,14 @@ export default declare(api => {
         path,
         {
           file: {
-            opts: { filename }
-          }
-        }
+            opts: { filename },
+          },
+        },
       ) {
         if (
           // removed `/` to make it work under Windows
-          filename.endsWith('html_re.js') &&
-          path.node.value === '[a-zA-Z_:][a-zA-Z0-9:._-]*'
+          filename.endsWith('html_re.js')
+          && path.node.value === '[a-zA-Z_:][a-zA-Z0-9:._-]*'
         ) {
           console.log('support @ in attribute names!')
           path.node.value = '[a-zA-Z@_:][a-zA-Z0-9:._-]*'
@@ -25,8 +25,8 @@ export default declare(api => {
       },
       ArrayExpression(path) {
         if (
-          path.parent.type === 'VariableDeclarator' &&
-          path.parent.id.name === 'HTML_SEQUENCES'
+          path.parent.type === 'VariableDeclarator'
+          && path.parent.id.name === 'HTML_SEQUENCES'
         ) {
           // Using Vue components at top-level
           // Previously `<foo-bar>` is transformed to `<p><foo-bar></p>`
@@ -38,18 +38,18 @@ export default declare(api => {
             t.arrayExpression([
               t.regExpLiteral('^<[A-Z]'),
               t.regExpLiteral('>'),
-              t.booleanLiteral(true)
+              t.booleanLiteral(true),
             ]),
             // custom elements with hyphens
             t.arrayExpression([
               t.regExpLiteral('^<\\w+\\-'),
               t.regExpLiteral('>'),
-              t.booleanLiteral(true)
+              t.booleanLiteral(true),
             ]),
-            ...path.node.elements.slice(-2)
+            ...path.node.elements.slice(-2),
           ]
         }
-      }
-    }
+      },
+    },
   }
 })
--- packages/@alterjs/saber-markdown/index.ts
+++ packages/@alterjs/saber-markdown/index.ts
@@ -1,3 +1,3 @@
-import MarkdownIt from "markdown-it"
+import MarkdownIt from 'markdown-it'
 
 export default MarkdownIt
