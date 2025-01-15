697e5d7b8667149fd6b7482f7f458b42709108b6
Chawye Hsu <su+git@chawyehsu.com>
2025-01-15 20:06:45 +0800

    chore: lint
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- packages/@alterjs/saber-plugin-query-posts/rollup.config.mjs
+++ packages/@alterjs/saber-plugin-query-posts/rollup.config.mjs
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
-import pkg from './package.json' assert { type: "json" }
+import pkg from './package.json' assert { type: 'json' }
 
 export default {
   input: './src/index.js',
@@ -20,6 +20,6 @@ export default {
     commonjs(),
     nodeResolve(),
     json(),
-    terser()
+    terser(),
   ],
 }
