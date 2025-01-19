4acabb9860539cb702c0039039273da88e5bd332
Chawye Hsu <su+git@chawyehsu.com>
2025-01-19 18:35:25 +0800

    fix!: workaround vue ssr cannot resolve lazy load components in subdir
    related to https://github.com/vuejs/vue/issues/12924
    
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- packages/@alterjs/saber/src/utils/getFileNames.ts
+++ packages/@alterjs/saber/src/utils/getFileNames.ts
@@ -5,7 +5,13 @@
  */
 export default function getFileNames(useHash: boolean) {
   return {
-    js: useHash ? 'js/[name].[chunkhash:8].js' : 'js/[name].js',
+    // FIXME(chawyehsu):
+    // Ideally we want all js files to be placed in the `js/` subdirectory,
+    // but vue-server-renderer 2.x has a bug [1] with webpack 5 that it cannot
+    // resolve the correct path of the component that is imported using `import()`.
+    //
+    // [1]: https://github.com/vuejs/vue/issues/12924
+    js: useHash ? '[name].[chunkhash:8].js' : '[name].js',
     css: useHash ? 'css/[name].[chunkhash:8].css' : 'css/[name].css',
     font: useHash ? 'fonts/[name].[hash:8].[ext]' : 'fonts/[path][name].[ext]',
     image: useHash
