6232c0492e6967a0832ce48a994897507833a26d
Chawye Hsu <su+git@chawyehsu.com>
2025-06-04 23:00:31 +0800

    chore(examples): lint
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- examples/vue-composition-api/pages/index.vue
+++ examples/vue-composition-api/pages/index.vue
@@ -1,6 +1,6 @@
 <script>
 // Using Vue 3.x composition API in Vue 2.x
-import { computed, reactive, defineComponent } from 'vue'
+import { computed, defineComponent, reactive } from 'vue'
 
 // Wrapping component definition with `defineComponent`
 // is only required for TypeScript users
